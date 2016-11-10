92beb3737c158bc2c1ea61dff613b89471280642
Andrey Pindyuk <a913000@gmail.com>
2016-11-10 10:06:05 +0700

    skb task 2b

--- src/index.js
+++ src/index.js
@@ -16,6 +16,35 @@ app.get('/task2a', (req,res,next) => {
   res.send(sum.toString());
 });
 
+app.get('/task2b', (req,res,next) => {
+  let fullname = (req.query.fullname || 'Invalid fullname');
+  fullname = fullname.split(' ');
+  switch (fullname.length) {
+    case 3: {
+      const lastName = fullname[2];
+      const firstName = fullname[0][0] + '.';
+      const patronymicName = fullname[1][0] + '.';
+      res.send(lastName + ' ' + firstName + ' ' + patronymicName);
+      return;
+    }
+    case 2: {
+      const lastName = fullname[1];
+      const firstName = fullname[0][0] + '.';
+      res.send(lastName + ' ' + firstName + ' ');
+      return;
+    }
+    case 1: {
+      const lastName = fullname[0];
+      res.send(lastName);
+      return;
+    }
+    default: {
+      res.send('Invalid fullname');
+      return;
+    }
+  }
+});
+
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
