613919d79f003004d00b5ba09f27bc747ed7b47e
Andrey Pindyuk <a913000@gmail.com>
2016-11-10 09:26:23 +0700

    skb task 2a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
+import express from  'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
 function hello(name) {
   console.log(`Hello ${name}`); // eslint-disable-line
 }
 hello('JS World');
+
+
+
+app.get('/task2a', (req,res,next) => {
+  const sum = (+req.query.a || 0) + (+req.query.b || 0);
+  res.send(sum.toString());
+});
+
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
